69ad06f90a4c91c333dbe69e44f3214531484846
suyash-03 <suyash.singh9450@gmail.com>
2021-06-20 19:31:29 +0530

    SimplePass - A Simple Password Manager to generate and store random safe passwords

--- lib/Screens/savedPasswords.dart
+++ lib/Screens/savedPasswords.dart
@@ -11,6 +11,7 @@ import 'package:password_generator/shared/loading_pulse.dart';
 
 class SavedPasswords extends StatefulWidget {
   final void Function() pageButtonPress;
+
   SavedPasswords(this.pageButtonPress);
 
   @override
@@ -18,175 +19,176 @@ class SavedPasswords extends StatefulWidget {
 }
 
 class _SavedPasswordsState extends State<SavedPasswords> {
-  Future<QuerySnapshot> passwordList;
-  bool isObscure=false;
 
+  final Stream<QuerySnapshot> _passwordsStream = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .collection('savedPasswords')
+      .snapshots();
 
-  Future<QuerySnapshot> getPasswordList() async{
-    CollectionReference passwords = FirebaseFirestore.instance
-          .collection('users')
-          .doc(FirebaseAuth.instance.currentUser.uid)
-          .collection('savedPasswords');
-    passwordList = passwords.get();
-    return passwordList;
-  }
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Color(0xfffbe1e4),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 5),
-                child: IconButton(icon: FaIcon(FontAwesomeIcons.backward,), onPressed: (){
-                  widget.pageButtonPress();
-                }),
-              ),
-              Padding(padding: EdgeInsets.fromLTRB(0, 0, 20, 5),
-                child: IconButton(icon: FaIcon(FontAwesomeIcons.plusCircle),
-                onPressed: (){
-                  showModalBottomSheet<dynamic>(context: context, shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+      body: Column(children: <Widget>[
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 5),
+              child: IconButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.backward,
                   ),
+                  onPressed: () {
+                    widget.pageButtonPress();
+                  }),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 20, 5),
+              child: IconButton(
+                icon: FaIcon(FontAwesomeIcons.plusCircle),
+                onPressed: () {
+                  showModalBottomSheet<dynamic>(
+                      context: context,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(30.0)),
+                      ),
                       isScrollControlled: true,
                       isDismissible: true,
-                      builder: (BuildContext context){
+                      builder: (BuildContext context) {
                         return ManualPasswordAdd();
                       });
-                },),
-              )
-            ],
-          ),
-          FadeInDown(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-              child: Row(
-                children: [
-                  Align(
-                    child: Text(
-                      "Saved Passwords:",
-                      style: rotateTextStyleHeading2,
-                    ),
-                  ),
-                ],
+                },
               ),
+            )
+          ],
+        ),
+         Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Row(
+              children: [
+                Align(
+                  child: Text(
+                    "Saved Passwords:",
+                    style: rotateTextStyleHeading2,
+                  ),
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: FutureBuilder<QuerySnapshot>(
-              future: getPasswordList(), builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: snapshot.data.docs.length,
-                      itemBuilder: (context, index) {
-                        Map passwordData = snapshot.data.docs[index].data();
-                        TextEditingController _controller = TextEditingController();
-                        _controller.text = passwordData['Password'];
-                        return ZoomIn(
-                            child: Padding(
-                              padding: const EdgeInsets.fromLTRB(20, 0, 20, 2),
-                              child: Container(
-                                height: 180,
-                                child: Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
-                                            child: Align(
-                                                alignment: Alignment.topLeft,
-                                                child: Text(
-                                                  passwordData['Website'], style: rotateTextStylePink,
-                                                )),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
-                                            child: IconButton(icon: FaIcon(FontAwesomeIcons.trashAlt,color: Colors.red,),
-                                                onPressed: () async
-                                                {
-                                                  //List Delete and Update
-                                                  await FirebaseFirestore.instance.runTransaction((Transaction myTransaction) async {
-                                                     myTransaction.delete(snapshot.data.docs[index].reference);
-                                                  }).then((value) {
-                                                    setState(() {
-                                                      
-                                                    });
-                                                  });
-                                                }),
-                                          )
-                                        ],
+        Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+          stream: _passwordsStream,
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Loading();
+            }
+            return new ListView(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                final TextEditingController _controller = TextEditingController();
+                _controller.text = data['Password'];
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 5),
+                  child: ZoomIn(
+                    child: new Container(
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+                                child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Container(
+                                      width: 200,
+                                      child: Text(
+                                        data['Website'], style: rotateTextStylePink,
                                       ),
-                                      Container(
-                                          child: Padding(
-                                            padding: const EdgeInsets.fromLTRB(30, 10, 30, 5),
-                                            child: TextField(
-                                              controller: _controller,
-                                              readOnly: true,
-                                              enableInteractiveSelection: false,
-                                              decoration: new InputDecoration(
-                                                suffixIcon: IconButton(
-                                                  icon: FaIcon(
-                                                    FontAwesomeIcons.copy,
-                                                  ),
-                                                  onPressed: () {
-                                                    final data = ClipboardData(
-                                                        text: _controller.text);
-                                                    Clipboard.setData(data);
-                                                    final snackBar = SnackBar(
-                                                      content: Text(
-                                                        "Password Copied to Clipboard",
-                                                        style: rotateTextStyle,
-                                                      ),
-                                                      backgroundColor:
-                                                      Color(0xffe79aad),
-                                                    );
-                                                    Scaffold.of(context)
-                                                      ..removeCurrentSnackBar()
-                                                      ..showSnackBar(snackBar);
-                                                    },
-                                                ),
-                                                border: OutlineInputBorder(
-                                                    borderRadius: BorderRadius.all(
-                                                        Radius.circular(50)),
-                                                    borderSide:
-                                                    BorderSide(color: Colors.pink)),
-                                                fillColor: Colors.white,
-                                                filled: true,
-                                              ),
-                                            ),
-                                          )),
-                                      Align(alignment: Alignment.centerLeft,
-                                          child: Padding(
-                                            padding: const EdgeInsets.fromLTRB(40, 2, 0, 10),
-                                            child: Text(passwordData['DateTime'],style: smallTextItalic,),
-                                          ))
-                                    ],
+                                    )),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+                                child: IconButton(
+                                    icon: FaIcon(FontAwesomeIcons.trashAlt,color: Colors.red,),
+                                    onPressed: () async {
+                                      FirebaseFirestore.instance.collection('users').
+                                      doc(FirebaseAuth.instance.currentUser.uid).
+                                      collection('savedPasswords').doc(document.id).delete();
+                                    }
+                                    ),
+                              )
+                            ],
+                          ),
+                          Container(
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(30, 10, 30, 5),
+                                child: TextField(
+                                  controller: _controller,
+                                  readOnly: true,
+                                  enableInteractiveSelection: false,
+                                  decoration: new InputDecoration(
+                                    suffixIcon: IconButton(
+                                      icon: FaIcon(
+                                        FontAwesomeIcons.copy,
+                                      ),
+                                      onPressed: () {
+                                        final data = ClipboardData(
+                                            text: _controller.text);
+                                        Clipboard.setData(data);
+                                        final snackBar = SnackBar(
+                                          content: Text(
+                                            "Password Copied to Clipboard",
+                                            style: rotateTextStyle,
+                                          ),
+                                          backgroundColor:
+                                          Color(0xffe79aad),
+                                        );
+                                        Scaffold.of(context)
+                                          ..removeCurrentSnackBar()
+                                          ..showSnackBar(snackBar);
+                                      },
+                                    ),
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(50)),
+                                        borderSide:
+                                        BorderSide(color: Colors.pink)),
+                                    fillColor: Colors.white,
+                                    filled: true,
                                   ),
                                 ),
-                              ),
-                            ));
-                      });
-                } else if (snapshot.connectionState == ConnectionState.waiting)
-                  return Loading();
-                else
-                  return Text("Cant Load");
-                },
-            ),
-          ),
-        ],
-      ),
+                              )),
+                          Align(alignment: Alignment.centerLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(40, 2, 0, 10),
+                                child: Text(data['DateTime'],style: smallTextItalic,),
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+          },
+        )),
+      ]),
     );
   }
 }
